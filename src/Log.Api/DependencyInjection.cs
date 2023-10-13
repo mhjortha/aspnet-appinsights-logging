3e1206f7226d0f5e1da41f075307378436354ae7
Michael Andersen Hjorth <mah@edgemo.com>
2023-10-13 10:10:50 +0200

    Some refactoring

---
+++ src/Log.Api/DependencyInjection.cs
@@ -0,0 +1,54 @@
+﻿using Log.Api.Data;
+using Log.Api.Persistence;
+using Log.Api.Services;
+using Microsoft.EntityFrameworkCore;
+
+namespace Log.Api;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddPresentation(
+        this IServiceCollection services, 
+        IConfiguration configuration
+        )
+    {
+        // Add services to the container.
+        services.AddControllers();
+        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+        services.AddEndpointsApiExplorer();
+        services.AddSwaggerGen();
+
+        // Adding DbContext
+        services.AddDbContext<ApplicationDbContext>(options =>
+        {
+            var connectionString = configuration.GetConnectionString("DefaultConnection");
+            options.UseSqlServer(connectionString);
+        });
+
+        // Adding Repositories
+        services.AddScoped<IUserRepository, UserRepository>();
+
+        // Adding Cors
+        services.AddCors(options =>
+        {
+            options.AddPolicy("AllowFrontend", policy =>
+            {
+                policy.AllowAnyOrigin()
+                    .AllowAnyHeader()
+                    .AllowAnyMethod();
+            });
+        });
+
+        // Takes connectionstring from appsettings - ApplicationInsights section as default
+        services.AddApplicationInsightsTelemetry(options =>
+        {
+            options.ConnectionString = configuration.GetValue<string>("ApplicationInsights:ConnectionString");
+        });
+
+        // Adding Logging Providers
+        services.AddSingleton(typeof(ILoggerAdapter<>), typeof(LoggerAdapter<>));
+        services.AddSingleton<ITelemetryHandler, TelemetryHandler>();
+
+        return services;
+    }
+}
--- src/Log.Api/Program.cs
+++ src/Log.Api/Program.cs
@@ -1,42 +1,12 @@
-using Log.Api.Data;
-using Log.Api.Persistence;
-using Log.Api.Services;
+using Log.Api;
 using Microsoft.ApplicationInsights.Extensibility;
-using Microsoft.EntityFrameworkCore;
 using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddPresentation(builder.Configuration);
 
-// Adding DbContext
-builder.Services.AddDbContext<ApplicationDbContext>(options =>
-{
-    var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
-    options.UseSqlServer(connectionString);
-});
-
-// Adding Repositories
-builder.Services.AddScoped<IUserRepository, UserRepository>();
-
-// Adding Cors
-builder.Services.AddCors(options =>
-{
-    options.AddPolicy("AllowFrontend", policy =>
-    {
-        policy.AllowAnyOrigin()
-            .AllowAnyHeader()
-            .AllowAnyMethod();
-    });
-});
-
-//Takes connectionstring from appsettings - ApplicationInsights section
-builder.Services.AddApplicationInsightsTelemetry();
-
+//Add serilog
 builder.Host.UseSerilog((hostingContext, loggerConfiguration) => loggerConfiguration
     .ReadFrom.Configuration(hostingContext.Configuration)
     .WriteTo.ApplicationInsights(new TelemetryConfiguration
@@ -45,24 +15,14 @@ builder.Host.UseSerilog((hostingContext, loggerConfiguration) => loggerConfigura
     },TelemetryConverter.Traces)
 );
 
-// Adding Logging Providers
-builder.Services.AddSingleton(typeof(ILoggerAdapter<>), typeof(LoggerAdapter<>));
-
-builder.Services.AddSingleton<ITelemetryHandler, TelemetryHandler>();
-
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 
 app.UseSwagger();
 app.UseSwaggerUI();
-
 app.UseHttpsRedirection();
-
 app.UseCors("AllowFrontend");
-
 app.UseAuthorization();
-
 app.MapControllers();
-
 app.Run();
